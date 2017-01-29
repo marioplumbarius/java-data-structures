46abe7e924b7cf5f77235a506c391fddc1872bc3
marioluan <mariosouzaluan@gmail.com>
2017-01-28 23:07:22 -0200

    created iterator for a generic linked list

---
+++ src/main/java/io/github/marioluan/datastructures/LinkedListIterator.java
@@ -0,0 +1,41 @@
+package io.github.marioluan.datastructures;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Implements an Iterator for a LinkedList data type.
+ * 
+ * @author marioluan
+ * @param <T>
+ *            the class type of the data from nodes
+ */
+public class LinkedListIterator<T extends Comparable<T>>
+        implements Iterator<T> {
+    private Node<T> cursor;
+
+    /**
+     * Constructs the iterator by pointing its cursor to the head.
+     */
+    public LinkedListIterator(Node<T> head) {
+        this.cursor = head;
+    }
+
+    public boolean hasNext() {
+        return this.cursor != null;
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
+    public T next() {
+        if (!hasNext())
+            throw new NoSuchElementException();
+
+        T oldItem = this.cursor.data;
+        this.cursor = this.cursor.next;
+
+        return oldItem;
+    }
+}
